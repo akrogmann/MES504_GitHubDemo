3624ff81ba0542df31a9eae6e9c68325daae9c0c
akrogmann <adamkrogs@gmail.com>
2026-04-15 11:38:23 -0400

    Filled out script exersice to demo github.

--- GitHubTutorialScript.R
+++ GitHubTutorialScript.R
@@ -1,5 +1,5 @@
-#INSERT YOUR NAME HERE
-#INSERT DATE HERE
+#Adam Krogmann
+#April 15, 2026
 
 #clear my workspace
 rm(list = ls())
@@ -11,10 +11,10 @@ library(learnr)
 #Generate random praise
 random_praise()
 #ADD COMMENT WITH YOUR RANDOM PRAISE PHRASE BELOW:
-
+#Terrific!
 #Generate random encouragement
 random_encouragement()
 #ADD COMMENT WITH YOUR RANDOM ENCOURAGEMENT PHRASE BELOW:
-
+#Try it again. Persevrance is the key to success.
 
 # commit and push your file to the GitHub repository
